485466002b4c05fdc7e19a26aa09a1a79750d463
Mary Falling <55334238+mfalling@users.noreply.github.com>
2021-02-17 15:48:18 -0500

    Update unlist_column.R
    Renaming parameter for clarity. It can take the column name as a string or the column number.

--- unlist_column.R
+++ unlist_column.R
@@ -1,16 +1,16 @@
 # Unlist a column in a dataframe.
 # Each list item gets inserted into its own row, retaining all other column values
 
-unlist_column <- function(dataset, colnum){
+unlist_column <- function(dataset, column){
   # Create an empty dataframe.
   df <- NULL
  
   # Iterate through each row in the dataframe.
   for (i in 1:nrow(dataset)){
     # Put each list item in its own row.
-    for(j in 1:(length(dataset[[colnum]][[i]]))){
+    for(j in 1:(length(dataset[[column]][[i]]))){
       row <- dataset[i, ]
-      row[[colnum]] <- dataset[[colnum]][[i]][[j]]
+      row[[colnum]] <- dataset[[column]][[i]][[j]]
      
       # Build the dataframe.
       df <- rbind(df, row)
